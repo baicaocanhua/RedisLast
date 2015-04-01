7438ebe3d438e3ca0d3ae5f708276bdc672c8d01
baicaocanhua <330557864@qq.com>
2015-04-01 17:17:48 +0800

    modelnew

---
+++ RedisLast/Model/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedisLast.Model
+{
+    public class BlogPost
+    {
+        /// <summary>
+        /// 文章
+        /// </summary>
+        public BlogPost()
+        {
+            this.Categories = new List<string>();
+            this.Tags = new List<string>();
+            this.Comments = new List<BlogPostComment>();
+        }
+
+        public long Id { get; set; }
+        public long BlogId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> Categories { get; set; }
+        public List<string> Tags { get; set; }
+        public List<BlogPostComment> Comments { get; set; }
+    }
+}
---
+++ RedisLast/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedisLast.Model
+{
+    public class User
+    {
+        public User()
+        {
+            this.BlogIds = new List<long>();
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<long> BlogIds { get; set; }
+    }
+}
