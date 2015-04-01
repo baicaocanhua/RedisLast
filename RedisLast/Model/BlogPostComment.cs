dff7faf3ec965268d84cc4388bba01fb91629616
baicaocanhua <330557864@qq.com>
2015-04-01 16:52:59 +0800

    blogTest

---
+++ RedisLast/Model/BlogPostComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedisLast.Model
+{
+    public class BlogPostComment
+    {
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
